d935bedd87a1b7c9d1391faf584399eb1ee3e13c
Mauricio Figueroa <mauricio.figueroa@farfetched.dev>
2024-10-26 21:03:55 -0500

    fixed load py
    modified script so it will load the correct brain

--- ReinforcementLearning/Source/exp_3/load.py
+++ ReinforcementLearning/Source/exp_3/load.py
@@ -5,13 +5,13 @@ from environment.SGE import SnakeGameEnv
 # Note: You should be running this script from a system cmd that has been cd'd into the workspace directory with the appropriate env activated.
 # The parameters here are to select which model to load.
 main_dir    = "./Source/exp_3/output_data"
-date_str    = "01-14-24"    # mod this to specify the model date
-env_str     = "SGE-1"     # ... env version
+date_str    = "01-17-24"    # mod this to specify the model date
+env_str     = "SGE-18"     # ... env version
 algo_str    = "PPO"         # ... algorithm
-run_str     = "r1"          # ... run
-time_str    = "4980000"      # ... checkpoint
+run_str     = "r3"          # ... run
+time_str    = "10000"      # ... checkpoint
 
-models_dir = main_dir + "/loadable_models" + "/" + date_str + "/" + env_str + "/" + algo_str + "/" + run_str + "/" + time_str + ".zip"
+models_dir = main_dir + "/" + time_str + ".zip"
 
 # Create the env
 # Note: render_mode="human" will let you see what's happening as it trains, but training will be significantly slower.
